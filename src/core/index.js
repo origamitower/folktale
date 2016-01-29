f45a00990f169433b4d6e4a3b2d2c72337e5382a
Quildreen Motta <quildreen.ribeiro@plansis.com.br>
2016-01-29 12:01:50 -0200

    feat: Adds entry-point to the `core` module

---
+++ src/core/index.js
@@ -0,0 +1,38 @@
+//----------------------------------------------------------------------
+//
+// This source file is part of the Folktale project.
+//
+// Copyright (C) 2015-2016 Quildreen Motta.
+// Licensed under the MIT licence.
+//
+// See LICENCE for licence information.
+// See CONTRIBUTORS for the list of contributors to the project.
+//
+//----------------------------------------------------------------------
+
+module.exports = {
+  lambda: require('./lambda')
+};
+
+
+// -- Annotations ------------------------------------------------------
+if (process.env.NODE_ENV !== "production") {
+  require('metamagical/decorators')(
+    module.exports,
+    {
+      name: 'module folktale/core',
+      category: 'Functional Programming',
+      stability: 'stable',
+      platforms: ['ECMAScript'],
+      authors: ['Quildreen Motta'],
+      module: 'folktale/core',
+      licence: 'MIT',
+      documentation: `
+The \`folktale.core\` module provides essential building blocks and
+compositional operations which are likely to be used by most programs,
+and which don't quite fit in more specific categories (like the \`data\`
+module).
+      `
+    }
+  );
+}
