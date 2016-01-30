c73e150a22e527549f9ff18837fef0857acd24f4
Quildreen Motta <quildreen.ribeiro@plansis.com.br>
2016-01-30 17:17:17 -0200

    feat: Adds entry-point to core.object module.

---
+++ src/core/object/index.js
@@ -0,0 +1,44 @@
+//----------------------------------------------------------------------
+//
+// This source file is part of the Folktale project.
+//
+// Copyright (C) 2015-2016 Quildreen Motta.
+// Licensed under the MIT licence.
+//
+// See LICENCE for licence information.
+// See CONTRIBUTORS for the list of contributors to the project.
+//
+//----------------------------------------------------------------------
+
+module.exports = {
+  infix: require('./infix'),
+  entries: require('./entries'),
+  mapEntries: require('./map-entries'),
+  mapValues: require('./map-values'),
+  values: require('./values')
+};
+
+
+// -- Annotations ------------------------------------------------------
+if (process.env.NODE_ENV !== "production") {
+  module.exports[Symbol.for('@@meta:magical')] = {
+    name: 'module folktale/core/object',
+    category: 'Utilities for Native Values',
+    stability: 'stable',
+    platforms: ['ECMAScript'],
+    authors: ['Quildreen Motta'],
+    module: 'folktale/core/object',
+    licence: 'MIT',
+    documentation: `
+Provides missing utilities for handling objects as dictionaries.
+
+
+## Why?
+
+Idiomatic JS code tends to use plain objects as records or dictionaries,
+but there aren't many native facilities for handling them in that way.
+This module tries to fill that gap.
+    `
+  };
+}
+
