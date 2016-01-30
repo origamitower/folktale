1e1e02e5d1dc6fff45ea6eb18099eebe6f6822e2
Quildreen Motta <quildreen.ribeiro@plansis.com.br>
2016-01-30 12:35:11 -0200

    docs: Complete documentation for ADT module

--- src/core/adt/index.js
+++ src/core/adt/index.js
@@ -10,7 +10,6 @@
 //
 //----------------------------------------------------------------------
 
-
 // All ADT objects inherit from ADT, so this allows common behaviour
 // to be shared here.
 const ADT = {
@@ -37,7 +36,8 @@ const mapObject = (object, transform) =>
 // A pattern is an object in the form of `{ String -> [String] }`,
 // and this function returns an object of the form:
 //
-//     { name: string,
+//     interface Variant {
+//       name: string,
 //       constructor: Constructor,
 //       prototype: Object,
 //       fields: [String]
@@ -56,7 +56,7 @@ const defineVariants = (patterns, namespace) => {
         this[key] = data[key];
       }
     };
-    ADT_Variant.prototype = Object.create(namespace.prototype);
+    ADT_Variant.prototype = Object.create(namespace);
     Object.assign(ADT_Variant.prototype, {
       ['is' + name]: true,
       tag: name,
@@ -78,14 +78,10 @@ const defineVariants = (patterns, namespace) => {
 
 // Given a set of patterns, constructs an ADT structure.
 const data = (patterns) => {
-  function ADT_Namespace() { };
+  const ADT_Namespace = Object.create(ADT);
+  const variants      = defineVariants(patterns, ADT_Namespace);
 
-  const variants = defineVariants(patterns, ADT_Namespace);
-
-  ADT_Namespace.prototype = Object.create(ADT);
-  Object.assign(ADT_Namespace.prototype, {
-    variants
-  });
+  Object.assign(ADT_Namespace, { variants});
 
   return ADT_Namespace;
 };
@@ -230,7 +226,127 @@ support for JavaScript.
 
 ## Architecture
 
+In most cases, one would ideally want a closed sum (that is, once you
+construct an ADT, there's no way of changing the variants that are part
+of it). JavaScript is an untyped language, however, so that doesn't make
+much of a sense to begin with.
 
+Because of this, the ADT module approaches the problem in a structural-type-ish
+way, which happens to be very similar to how different values are handled in
+unityped languages to begin with. In essence, calling \`data\` with a set
+of patterns results in the creation of N constructors, each with a distinct
+**tag**. The constructors accept an object as their only argument, and will
+extract the fields that are relevant to that variant to initialise its state.
+
+Revisiting the previous \`List\` ADT example, we can see this by just looking
+at its return value:
+
+    const List = data({
+      Nil: [],
+      Cons: ['head', 'tail']
+    });
+
+Would expand to:
+
+    const List = Object.create(ADT);
+
+    List.Nil = function() { };
+    List.Nil.prototype = Object.create(List);
+    List.Nil.prototype.tag = 'Nil';
+    List.Nil.prototype.fields = [];
+
+    List.Cons = function(data) {
+      this.head = data.head;
+      this.tail = data.tail;
+    }
+    List.Cons.prototype = Object.create(List);
+    List.Cons.prototype.tag = 'Cons';
+    List.Cons.prototype.fields = ['head', 'tail'];
+
+Furthermore, each variant gets a \`cata\` method for free. This method
+implements a kind of structural transformation (*catamorphism*) that
+is similar to a limited form of pattern matching:
+
+    const list = new Cons({ head: 1, tail: new Nil() });
+
+    const sum = (list) =>
+      list.cata({
+        Nil : _                => 0,
+        Cons: ({ head, tail }) => head + sum(tail)
+      });
+
+
+## Reflection and extension
+
+Structures created by the ADT module are reflective, so one can use the
+exposed information for meta-programming at runtime. One of the uses for
+this is providing a particular set of behaviours automatically for the
+structure — a process we call “derivation”. Things like \`equality\` or
+\`serialisation\` become pretty simple to do automatically, and relieve
+the burden on the user.
+
+Each ADT comes with a [[derive]] method, which will take one or more
+derivation functions and apply them to each variant in the structure.
+This makes it easy to implement derivations that work across all of
+the variants at once.
+
+Another approach is to attach methods to the ADT namespace itself, since
+all variants inherit from it. If you were to implement something like
+the \`cata\` operation, for example, you could do so as follows:
+
+    const List = data({
+      Nil: [],
+      Cons: ['head', 'tail']
+    });
+
+    List.match = function(pattern) {
+      pattern[this.tag](this)
+    }
+
+    const head = (list) => list.match({
+      Nil : _          => 'Nils have no head',
+      Cons: ({ head }) => head
+    });
+
+    head(new Nil());
+    // => 'Nils have no head'
+
+    head(new Cons({ head: 1, tail: new Nil() }));
+    // => 1
+
+
+## Membership testing
+
+Most of the time, it's a good idea to use structural equivalence and
+the \`catamorphism\` operation to deal with transformations on ADT
+structures. The dynamic dispatch in JavaScript will make sure that
+the right operations are invoked for you, with no work necessary
+in your part, and you don't risk any problems with Realms and other
+concepts that might give you different instances of the same object.
+
+Sometimes, however, it might be desirable to test if a variant
+belongs to a particular tagged union. ADT itself doesn't provide
+any method for this, but since variants inherit from the ADT
+namespace, you can use JavaScript's native \`.isPrototypeOf\`
+operation to test for this:
+
+    const ListA = data({
+      Nil: [],
+      Cons: ['head', 'tail']
+    });
+
+    const ListB = data({
+      Nil: [],
+      Cons: ['head', 'tail']
+    });
+
+    const b = new ListB.Nil();
+
+    ListA.isPrototypeOf(b);
+    // => false
+
+    ListB.isPrototypeOf(b);
+    // => true
     `
   };
 }
