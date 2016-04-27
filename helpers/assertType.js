21f82d8058335596a3fdc9b66ee96899410be9ee
Boris Marinov <marinovboris@gmail.com>
2016-04-27 17:41:17 +0300

    fix: add missing file

---
+++ helpers/assertType.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (type) {
+   var typeName = arguments.length <= 1 || arguments[1] === undefined ? 'Folktale ADT' : arguments[1];
+   return function (method, value) {
+      if (process.env.NODE_ENV !== 'production' && !type.isPrototypeOf(value)) {
+         console.warn('\n' + method + ' expects a value of the same type, but was given ' + value + '.\n\nThis could mean that you\'ve provided the wrong value to the method, in\nwhich case this is a bug in your program, and you should try to track\ndown why the wrong value is getting here.\n\nBut this could also mean that you have more than one ' + typeName + ' library\ninstantiated in your program. This is not **necessarily** a bug, it\ncould happen for several reasons:\n\n 1) You\'re loading the library in Node, and Node\'s cache didn\'t give\n    you back the same instance you had previously requested.\n\n 2) You have more than one Code Realm in your program, and objects\n    created from the same library, in different realms, are interacting.\n\n 3) You have a version conflict of folktale libraries, and objects\n    created from different versions of the library are interacting.\n\nIf your situation fits the cases (1) or (2), you are okay, as long as\nthe objects originate from the same version of the library. Folktale\ndoes not rely on reference checking, only structural checking. However\nyou\'ll want to watch out if you\'re modifying the ' + typeName + '\'s prototype,\nbecause you\'ll have more than one of them, and you\'ll want to make\nsure you do the same change in all of them — ideally you shouldn\'t\nbe modifying the object, though.\n\nIf your situation fits the case (3), you are *probably* okay if the\nversion difference isn\'t a major one. However, at this point the\nbehaviour of your program using ' + typeName + ' is undefined, and you should\ntry looking into why the version conflict is happening.\n\nParametric modules can help ensuring your program only has a single\ninstance of the folktale library. Check out the Folktale Architecture\ndocumentation for more information.\n    ');
+      }
+   };
+};
