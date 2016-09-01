e9cef44e8a1b49cf800e1f2d382e60e09689f975
Quildreen Motta <queen@robotlolita.me>
2016-08-31 21:37:17 -0300

    (doc core) Adds documentation to the Core entrypoint

--- src/core/index.js
+++ src/core/index.js
@@ -8,24 +8,37 @@
 //----------------------------------------------------------------------
 
 /*~
- * Essential building blocks and compositional operations which are
- * likely to be used by most programs are provided by this module.
+ * Provides essential functionality for functional programs.
+ * 
+ * 
+ * ## What's in Core?
+ * 
+ * This module provides a range of features that are often used as
+ * foundational building blocks in functional programming. It also
+ * provides features for handling some common built-in JavaScript
+ * structures in a functional way.
+ * 
+ * The operations are divided as follows:
+ * 
+ *   - **Modelling Data**: The Core.ADT module provides an
+ *     implementation of Tagged Unions with limited pattern matching.
+ *     This can be used to model your application's data more accurately,
+ *     and thus make it harder to misuse that data;
+ * 
+ *   - **Combining Functions**: The Core.Lambda module provides
+ *     operations on functions that allow combining functions together
+ *     (like `compose`) or changing how you apply functions (like `curry`
+ *     or `partialise`);
+ * 
+ *   - **Working with Objects as Dictionaries**: The Core.Object module
+ *     provides operations that let you treat regular JavaScript objects
+ *     as dictionaries. Several JavaScript APIs expect that usage of
+ *     objects, but the built-in operations don't support  this well,
+ *     so the Core.Object module addresses that.
  *
- * These operations usually don't fit in more specific categories,
- * like `data`, or provide a better alternative to functionality
- * that's already provided by the language itself.
- *
- * --------------------------------------------------------------------
+ * ---
  * name        : module folktale/core
- * module      : folktale/core
- * copyright   : (c) 2015-2016 Quildreen Motta, and CONTRIBUTORS
- * licence     : MIT
- * repository  : https://github.com/origamitower/folktale
- *
  * category    : Essential Operations
- *
- * maintainers:
- *   - Quildreen Motta <queen@robotlolita.me>
  */
 module.exports = {
   lambda: require('./lambda'),
