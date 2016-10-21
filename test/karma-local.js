3fc6f44d38b8ebdb2506c9f1be572b44841bdc13
Quildreen Motta <queen@robotlolita.me>
2016-10-21 00:26:29 -0200

    (meta) Only runs browser tests in PhantomJS

--- test/karma-local.js
+++ test/karma-local.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
