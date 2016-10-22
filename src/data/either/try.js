5676d39238e821925eb292e47d3328c429ff07e7
Quildreen Motta <queen@robotlolita.me>
2016-10-22 14:00:07 -0200

    (fix) Changes the signature for Either.try, now taking a thunk

--- src/data/either/try.js
+++ src/data/either/try.js
@@ -3,6 +3,22 @@ const { Left, Right } = require('./either');
 /*~
  * Runs a function that may raise an exception, trapping it, and returning
  * an Either representing the result.
+ * 
+ * ## Example::
+ * 
+ *     function succ(nat) {
+ *       if (nat < 0) {
+ *         throw `Not a natural number: ${nat}`;
+ *       } else {
+ *         return nat + 1;
+ *       }
+ *     }
+ * 
+ *     const { Left, Right } = require('folktale/data/either');
+ *     
+ *     _try(() => succ(-1));  // ==> Left('Not a natural number: -1')
+ *     _try(() => succ(1));   // ==> Right(2)
+ * 
  * ---
  * category: Handling exceptions
  * stability: experimental
@@ -10,11 +26,11 @@ const { Left, Right } = require('./either');
  *   - "@boris-marinov"
  * 
  * type: |
- *   forall a, b: ((Any...) => b :: throws a) => Either a b
+ *   forall a, b: (() => b :: throws a) => Either a b
  */
-const _try = (f) => (...args) => {
+const _try = (f) => {
   try {
-    return Right(f(...args));
+    return Right(f());
   } catch (e) {
     return Left(e);
   }
