1f4127e6bafe4d48a492ebba867bff45c179f6a9
Quildreen Motta <queen@robotlolita.me>
2016-10-23 13:04:07 -0200

    (docs) Documentation for Future

--- src/data/future/_future.js
+++ src/data/future/_future.js
@@ -20,6 +20,10 @@ const { Pending, Resolved, Rejected } = require('./_execution-state');
 /*~
  * Represents an eventual value, like Promise, but without a recursive
  * `.then`.
+ * 
+ * ---
+ * category: Asynchronous concurrency
+ * stability: experimental
  */
 class Future {
   constructor() {
@@ -60,8 +64,22 @@ class Future {
   /*~
    * Attaches a listener to be invoked when the Future resolves.
    * 
+   * ## Example::
+   * 
+   *     const Future = require('folktale/data/future');
+   *     let result = null;
+   * 
+   *     Future.of(1).listen({
+   *       onCancelled: ()      => { result = 'cancelled' },
+   *       onResolved:  (value) => { result = `resolved: ${value}` },
+   *       onRejected:  (value) => { result = `rejected: ${value}` }
+   *     });
+   * 
+   *     result; // ==> 'resolved: 1'
+   * 
    * ---
    * category: Reacting to Future events
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(DeferredListener 'f 's) => Future 'f 's
    */
@@ -78,10 +96,18 @@ class Future {
 
   // --[ Transforming Futures ]----------------------------------------
   /*~
-   * Transforms the value inside a future with a monadic function.
+   * Transforms resolved futures into new futures.
+   * 
+   * ## Example::
+   * 
+   *     const Future = require('folktale/data/future');
+   * 
+   *     Future.of(1).chain(x => Future.of(x + 1));        // => Future.of(2)
+   *     Future.rejected(1).chain(x => Future.of(x + 1));  // => Future.rejected(1)
    * 
    * ---
    * category: Transforming futures
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(('s) => Future 's2) => Future 'f 's2
    */
@@ -103,10 +129,18 @@ class Future {
   }
 
   /*~
-   * Transforms the value inside a future with a simple function.
+   * Transforms the value inside of a resolved future.
+   * 
+   * ## Example::
+   * 
+   *     const Future = require('folktale/data/future');
+   * 
+   *     Future.of(1).map(x => x + 1);        // => Future.of(2)
+   *     Future.rejected(1).map(x => x + 1);  // => Future.rejected(1)
    * 
    * ---
    * category: Transforming futures
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(('s) => 's2) => Future 'f 's2
    */
@@ -115,11 +149,21 @@ class Future {
   }
 
   /*~
-   * Transforms the value inside a future with a function contained in
-   * another future.
+   * Applies a function contained in the future to the value of another future.
+   * 
+   * ## Example::
+   * 
+   *     const Future = require('folktale/data/future');
+   * 
+   *     const inc = (x) => x + 1;
+   * 
+   *     Future.of(inc).apply(Future.of(1));        // => Future.of(2)
+   *     Future.of(inc).apply(Future.rejected(1));  // => Future.rejected(1)
+   *     Future.rejected(inc).apply(Future.of(1));  // => Future.rejected(inc)  
    * 
    * ---
    * category: Transforming futures
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(Future 'f (('s) => 's2)) => Future 'f 's2
    */
@@ -128,10 +172,21 @@ class Future {
   }
 
   /*~
-   * Transforms successes and failures in a future.
+   * Transforms the rejection or success of a future.
+   * 
+   * ## Example::
+   * 
+   *     const Future = require('folktale/data/future');
+   * 
+   *     const inc = (x) => x + 1;
+   *     const dec = (x) => x - 1;
+   * 
+   *     Future.of(1).bimap(inc, dec);        // => Future.of(dec(1))
+   *     Future.rejected(1).bimap(inc, dec);  // => Future.rejected(inc(1))
    * 
    * ---
    * category: Transforming futures
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(('f) => 'f2, ('s) => 's2) => Future 'f2 's2
    */
@@ -149,8 +204,16 @@ class Future {
   /*~
    * Transform the values of rejected futures.
    * 
+   * ## Example::
+   * 
+   *     const { of, rejected } = require('folktale/data/future');
+   * 
+   *     of(1).mapRejection(x => x + 1);        // => of(1)
+   *     rejected(1).mapRejection(x => x + 1);  // => rejected(2)
+   * 
    * ---
    * category: Transforming values
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(('f) => 'f2) => Future 'f2 's
    */
@@ -163,8 +226,17 @@ class Future {
   /*~
    * Transforms a rejected future into a new future.
    * 
+   * ## Example::
+   * 
+   *     const { of, rejected } = require('folktale/data/future');
+   * 
+   *     of(1).recover(x => of(x + 1));             // => of(1)
+   *     rejected(1).recover(x => of(x + 1));       // => of(2)
+   *     rejected(1).recover(x => rejected(x + 1)); // => rejected(2)
+   * 
    * ---
    * category: Recovering from errors
+   * stability: experimental
    * type: |
    *   (Future 'f 's).(('f) => Future 'f2 's2) => Future 'f2 's
    */
@@ -185,6 +257,37 @@ class Future {
     return deferred.future();
   }
 
+
+  /*~
+   * Returns a future that eventually transforms its state by invoking
+   * a function for that state. The function must return a new Future.
+   * 
+   * ## Example::
+   * 
+   *     const { of, rejected } = require('folktale/data/future');
+   * 
+   *     const pattern = {
+   *       Cancelled: ()      => of('cancelled'),
+   *       Resolved:  (value) => of(`resolved: ${value}`),
+   *       Rejected:  (value) => of(`rejected: ${value}`)
+   *     };
+   * 
+   *     of(1).willMatchWith(pattern);        // => of('resolved: 1')
+   *     rejected(1).willMatchWith(pattern);  // => of('rejected: 1')
+   *  
+   * ---
+   * category: Pattern matching
+   * stability: experimental
+   * type: |
+   *   forall a, b, c, d:
+   *     type Pattern = { r |
+   *       Cancelled: ()  => Future c d,
+   *       Resolved:  (b) => Future c d,
+   *       Rejected:  (a) => Future c d
+   *     }
+   *     
+   *     (Future a b).(Pattern) => Future c d 
+   */
   willMatchWith(pattern) {
     let deferred = new Deferred();
     const resolve = (handler) => (value) => handler(value).listen({
@@ -204,8 +307,16 @@ class Future {
   /*~
    * Transforms rejected futures in successes, and vice-versa.
    * 
+   * ## Example::
+   * 
+   *     const { of, rejected } = require('folktale/data/future');
+   * 
+   *     of(1).swap();         // => rejected(1)
+   *     rejected(1).swap();   // => of(1)
+   * 
    * ---
    * category: Recovering from errors
+   * stability: experimental
    * type: |
    *   (Future 'f 's).() => Future 's 'f
    */
@@ -225,8 +336,19 @@ class Future {
   /*~
    * Returns a textual representation of this object for debugging.
    * 
+   * ## Example::
+   * 
+   *     const { of, rejected } = require('folktale/data/future');
+   * 
+   *     of(1).toString(); 
+   *     // ==> 'folktale:Future(Resolved(1), 0 listeners)'
+   *     
+   *     rejected(1).toString();
+   *     // ==> 'folktale:Future(Rejected(1), 0 listeners)'
+   * 
    * ---
    * category: Debugging
+   * stability: experimental
    * type: |
    *   (Future 'f 's).() => String
    */
@@ -242,6 +364,7 @@ class Future {
    * 
    * ---
    * category: Debugging
+   * stability: experimental
    * type: |
    *   (Future 'f 's).() => String
    */
@@ -253,6 +376,16 @@ class Future {
 
 // ---[ Constructing futures ]-----------------------------------------
 Object.assign(Future, {
+  /*~
+   * Constructs a future containing a successful value.
+   * 
+   * ---
+   * category: Constructing futures
+   * stability: experimental
+   * type: |
+   *   forall a, b:
+   *     (Future).(b) => Future a b
+   */
   of(value) {
     let result = new Future();
     result._state = Resolved(value);
@@ -264,8 +397,9 @@ Object.assign(Future, {
    * 
    * ---
    * category: Constructing futures
+   * stability: experimental
    * type: |
-   *   (Future).('f) => Future 'f 's
+   *   forall a, b: (Future).(a) => Future a b
    */
   rejected(reason) {
     let result = new Future();
