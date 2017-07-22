0aa9343b5b4ffd2896727eaed43e644d9f478edb
Jesse Warden <jesse@jessewarden.com>
2017-07-22 11:01:04 -0400

    - corrected assertObject name - made statements on 1 line

--- packages/base/source/adt/union/union.js
+++ packages/base/source/adt/union/union.js
@@ -108,10 +108,14 @@ instead to check if a value belongs to the ADT variant.`);
        *   where 'b = 'a[`@@folktale:adt:tag]
        */
       matchWith(pattern) {
-        assertObject('Union.mapObject#matchWith', pattern);
-        if (name in pattern) { return pattern[name](this); }
-        else if (ANY in pattern) { return pattern[ANY]() }
-        else { throw new Error(getMatchWithErrorMessage(pattern, name)) }
+        assertObject('${name}#matchWith', pattern);
+        if (name in pattern) {
+          return pattern[name](this);
+        } else if (ANY in pattern) {
+          return pattern[ANY]();
+        } else {
+          throw new Error(getMatchWithErrorMessage(pattern, name));
+        } 
       }
     });
 
