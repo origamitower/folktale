c4c759f5fcc17485ccd4127a09eb059279c63151
Quildreen Motta <quildreen@gmail.com>
2017-10-07 23:17:02 -0300

    Partial types for concurrency modules

--- packages/base/index.d.ts
+++ packages/base/index.d.ts
@@ -75,9 +75,9 @@ declare namespace folktale {
 
   
   // -- Result
-  type Result<A, B> = Error<A, B> | Ok<A, B>;
+  type Result<A, B> = Err<A, B> | Ok<A, B>;
 
-  interface Error<A, B> {
+  interface Err<A, B> {
     __type0: A;
     __type1: B;
     __tag: 'Error';
@@ -185,11 +185,119 @@ declare namespace folktale {
     fromMaybe<A, B>(value: Maybe<B>, failure: A): Validation<A, B>;
   }
 
+  // -- Concurrency
+  interface Future<A, B> {
+    toPromise(): Promise<B>
+    inspect(): string
+    toString(): string
+
+    willMatchWith<C, D>(patterns: {
+      Cancelled(): Future<C, D>,
+      Resolved(value: B): Future<C, D>,
+      Rejected(reason: A): Future<C, D>
+    }): Future<C, D>
+
+    listen(patterns: {
+      onCancelled(): void,
+      onResolved(value: B): void,
+      onRejected(reason: A): void
+    }): Future<A, B>
+
+    orElse(handler: (_: A) => Future<A, B>): Future<A, B>
+    swap(): Future<B, A>
+    apply<C>(this: Future<A, (_: B) => C>, that: Future<A, B>): Future<A, C>
+    bimap<C, D>(onRejected: (_: A) => C, onResolved: (_: B) => D): Future<C, D>
+    chain<C>(f: (_: B) => Future<A, C>): Future<A, C>
+    map<C>(f: (_: B) => C): Future<A, C>
+    mapRejected<C>(f: (_: A) => C): Future<C, B>
+
+
+    /** @deprecated */
+    recover(handler: (_: A) => Future<A, B>): Future<A, B>
+  }
+
+  interface StaticFuture {
+    of<A, B>(value: B): Future<A, B>
+    rejected<A, B>(reason: A): Future<A, B>
+    fromPromise<B>(promise: Promise<B>): Future<Error, B>
+  }
+
+  interface TaskExecution<A, B> {
+    cacel(): TaskExecution<A, B>
+
+    listen(pattern: {
+      onCancelled(): void,
+      onResolved(value: B): void,
+      onRejected(reason: A): void
+    }): TaskExecution<A, B>
+
+    promise(): Promise<B>
+    future(): Future<A, B>
+  }
+
+  interface Task<A, B> {
+    and<B2>(that: Task<A, B2>): Task<A, [B, B2]>
+    or<B2>(that: Task<A, B2>): Task<A, B | B2>
+    run(): TaskExecution<A, B>
+    
+    willMatchWith<A2, B2>(patterns: {
+      Cancelled(): Task<A2, B2>,
+      Resolved(value: B): Task<A2, B2>,
+      Rejected(reason: A): Task<A2, B2>
+    }): Task<A2, B2>
+
+    orElse(handler: (_: A) => Task<A, B>): Task<A, B>
+    swap(): Task<B, A>
+    apply<B2>(this: Task<A, (_: B) => B2>, that: Task<A, B>): Task<A, B2>
+    bimap<A2, B2>(onRejected: (_: A) => A2, onResolved: (_: B) => B2): Task<A2, B2>
+    chain<B2>(f: (_: B) => Task<A, B2>): Task<A, B2>
+    map<B2>(f: (_: B) => B2): Task<A, B2>
+    mapRejected<A2>(f: (_: A) => A2): Task<A2, B>
+  }
+
+  interface StaticTask {
+    waitAll<E, V>(tasks: Task<E, V>[]): Task<E, V[]>
+    waitAny<E, V>(tasks: Task<E, V>[]): Task<E, V>
+    of<E, V>(value: V): Task<E, V>
+    rejected<E, V>(reason: E): Task<E, V>
+    task<E, V>(resolver: TaskResolver<E, V>): Task<E, V>
+    fromNodeback<E, V>(fn: (cb: (e: E, v: V) => void) => void): () =>  Task<E, V>
+    fromNodeback<A, E, V>(fn: (a1: A, cb: (e: E, v: V) => void) => void): (a1: A) =>  Task<E, V>
+    fromNodeback<A, A2, E, V>(fn: (a1: A, a2: A2, cb: (e: E, v: V) => void) => void): (a1: A, a2: A2) =>  Task<E, V>
+    fromNodeback<A, A2, A3, E, V>(fn: (a1: A, a2: A2, a3: A3, cb: (e: E, v: V) => void) => void): (a1: A, a2: A2, a3: A3) =>  Task<E, V>
+    fromNodeback<A, A2, A3, A4, E, V>(fn: (cb: (a1: A, a2: A2, a3: A3, a4: A4, e: E, v: V) => void) => void): (a1: A, a2: A2, a3: A3, a4: A4) =>  Task<E, V>
+    fromNodeback<A, A2, A3, A4, A5, E, V>(fn: (a1: A, a2: A2, a3: A3, a4: A4, a5: A5, cb: (e: E, v: V) => void) => void): (a1: A, a2: A2, a3: A3, a4: A4, a5: A5) =>  Task<E, V>
+    fromNodeback<A, A2, A3, A4, A5, A6, E, V>(fn: (a1: A, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, cb: (e: E, v: V) => void) => void): (a1: A, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) =>  Task<E, V>
+    
+    fromPromised<E, V>(fn: () => Promise<V>): () =>  Task<E, V>
+    fromPromised<A, E, V>(fn: (a1: A) => Promise<V>): (a1: A) =>  Task<E, V>
+    fromPromised<A, A2, E, V>(fn: (a1: A, a2: A2) => Promise<V>): (a1: A, a2: A2) =>  Task<E, V>
+    fromPromised<A, A2, A3, E, V>(fn: (a1: A, a2: A2, a3: A3) => Promise<V>): (a1: A, a2: A2, a3: A3) =>  Task<E, V>
+    fromPromised<A, A2, A3, A4, E, V>(fn: (a1: A, a2: A2, a3: A3, a4: A4) => Promise<V>): (a1: A, a2: A2, a3: A3, a4: A4) =>  Task<E, V>
+    fromPromised<A, A2, A3, A4, A5, E, V>(fn: (a1: A, a2: A2, a3: A3, a4: A4, a5: A5) => Promise<V>): (a1: A, a2: A2, a3: A3, a4: A4, a5: A5) =>  Task<E, V>
+    fromPromised<A, A2, A3, A4, A5, A6, E, V>(fn: (a1: A, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => Promise<V>): (a1: A, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) =>  Task<E, V>
+    
+    do<E, V>(generator: GeneratorFunction): Task<E, V>
+  }
+
+  interface TaskResolver<E, V> {
+    resolve(value: V): void
+    reject(reason: E): void
+    cancel(): void
+    cleanup(handler: () => void): void
+    onCancelled(handler: () => void): void
+    isCancelled: boolean
+  }
+
   // -- Entry point
   export const core: Core;
   export const maybe: StaticMaybe;
   export const result: StaticResult;
   export const validation: StaticValidation;
+  export const concurrency: {
+    task: StaticTask
+    future: StaticFuture
+  };
 }
 
 export = folktale
